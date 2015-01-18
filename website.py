8268a3816e474c270a97ef9a778ce370c0fd5063
MaTachi <dannie92@gmail.com>
2015-01-18 17:45:00 +0100

    Run the front-end with multiple threads

--- website.py
+++ website.py
@@ -38,4 +38,4 @@ def index():
 
 if __name__ == '__main__':
     host = sys.argv[1] if len(sys.argv) > 1 else '127.0.0.1'
-    app.run(debug=True, host=host)
+    app.run(debug=True, threaded=True, host=host)
